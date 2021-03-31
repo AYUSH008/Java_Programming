bba78eebd2d04cc7a5f13cd52219fddab4c8a7d3
AYUSH ASHATKAR <34589242+AYUSH008@users.noreply.github.com>
2021-03-31 23:30:52 +0530

    Updated Arrays Ops

--- RecursionPrint.java
+++ RecursionPrint.java
@@ -3,8 +3,10 @@ package Recursion;
 public class RecursionPrint {
     public  static  void main (String []args){
 
-      //  PrintReverse(5);
-        PrintReverseDe(5);
+        //PrintReverse(5) ;
+      //  PrintReverseDe(5);
+        //PDI(5);
+        PDISkip(5);
     }
 
     public static void PrintReverse(int n){
@@ -21,6 +23,28 @@ public class RecursionPrint {
         }
         PrintReverseDe(n-1);
         System.out.print(n+" ");
+    }
+    public static void PDI(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n+" ");
+        PDI(n-1);
+        System.out.println(n);
+    }
+    public static void PDISkip( int n)
+    {
+        if(n==0){
+            return ;
+        }
+        if(n%2==1){
+            System.out.println(n);
+        }
+        PDISkip(n-1);
+        if(n%2==0){
+            System.out.println(n);
+        }
+
     }
 
 }
