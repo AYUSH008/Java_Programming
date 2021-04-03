defb1245cc177de6089f8bd0645e5f7826b1c549
AYUSH ASHATKAR <34589242+AYUSH008@users.noreply.github.com>
2021-04-03 23:54:01 +0530

    Bubble Sort (using recursion)

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,28 +1,32 @@
-package OneDimensionalArray;
+package Recursion;
+
+import java.util.Arrays;
 
 public class BubbleSort {
+    public static void main(String[]args){
 
-    public static void main (String []args){
-        int [] arr = {99,88,77,66,55};
-        bubbleSort(arr);
-        display(arr);
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
 
+        bubbleSort(arr, arr.length);
+
+        System.out.println("Sorted array : ");
+        System.out.println(Arrays.toString(arr));
     }
-    public static void bubbleSort(int []arr){
-        for(int counter = 0; counter< arr.length-1;counter++){
-            for(int j=0;j< arr.length-1-counter;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
+    static void bubbleSort(int arr[], int n)
+    {
+        // Base case
+        if (n == 1)
+            return;
+
+        for (int i=0; i<n-1; i++)
+            if (arr[i] > arr[i+1])
+            {
+
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             }
-        }
-    }
-    public static void display(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
 
+        bubbleSort(arr, n-1);
+    }
 }
